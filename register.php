45202a20435316138a63800aeead2e973d889eac
Prajkta Gudle <124344569+Prajkta27@users.noreply.github.com>
2024-07-06 17:53:31 +0530

    register.php

---
+++ register.php
@@ -0,0 +1,16 @@
+<?php
+include 'db.php';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+if ($conn->query($sql) === TRUE) {
+    echo "Registration successful!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
