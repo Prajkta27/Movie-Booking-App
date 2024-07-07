b40b4fcf85b61e2e327efa7504dcf61c6b619828
Prajkta Gudle <124344569+Prajkta27@users.noreply.github.com>
2024-07-07 08:38:03 +0530

    movies.php

--- movies.php
+++ movies.php
@@ -1,3 +1,4 @@
+<!-- movies.php -->
 <?php
 include 'db.php';
 
@@ -9,7 +10,32 @@ while ($row = $result->fetch_assoc()) {
     $movies[] = $row;
 }
 
-echo json_encode($movies);
-
 $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <h1>Movies</h1>
+    <div id="moviesList">
+        <?php if (!empty($movies)): ?>
+            <ul>
+                <?php foreach ($movies as $movie): ?>
+                    <li><?php echo htmlspecialchars($movie['title']); ?></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php else: ?>
+            <p>No movies found.</p>
+        <?php endif; ?>
+    </div>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
+
+
